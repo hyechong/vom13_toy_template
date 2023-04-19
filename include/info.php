ff22e6166f3e3c922160a777aa602d33cff15d23
hyechong <ckgpwl2@gmail.com>
2023-04-19 09:37:26 +0900

    header mobile complete 0419

--- include/info.php
+++ include/info.php
@@ -23,7 +23,7 @@
     <li class="user"><a href="/common_template_0323/signin.php"><i class="ri-user-fill"></i></a></li>
   <?php } else { ?>
     <li class="loggedIn">
-      <span>toto님</span>&nbsp;|&nbsp;<a href="/common_template_0323/php/logout.php">Logout</a>
+      <span><?=$userid?>님</span>&nbsp;|&nbsp;<a href="/common_template_0323/php/logout.php">Logout</a>
     </li>
   <?php } ?>
 </ul>
